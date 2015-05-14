38ae5c7f1b314260baece4c0779c5412f9b4d12b
seanyarrall <sean.yarrall@gmail.com>
2015-05-14 12:16:51 +1200

    created project in visual studio

---
+++ RecipeBook/RecipeBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecipeBook.Startup))]
+namespace RecipeBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
