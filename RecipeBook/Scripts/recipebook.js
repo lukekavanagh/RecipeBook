7bcb118646fbf966cc862cf5d1d839c406140217
Rich Churcher <rich.churcher@gmail.com>
2015-05-15 14:27:47 +1200

    Category list display

--- RecipeBook/Scripts/recipebook.js
+++ RecipeBook/Scripts/recipebook.js
@@ -1,4 +1,21 @@
 ﻿$(document).ready(function() {
     $('#welcome').hide(300).delay(1000).slideToggle(30000);
     $('.cow_image').hide(300).delay(1000).slideToggle(30000);
+
+    var $categories = $.getJSON('api/categories', function(data) {
+        $.each(data, function(i, category) {
+            $('#categoryList')
+                .append('<a class="category" href="categories/' +
+                category["Id"] + '">' +
+                category["Name"] + '</a>');
+        });
+    });
+    var $recipes = $.getJSON('api/recipes', function(data) {
+        $.each(data, function(key, val) {
+            $.each(val, function(key, val) {
+                console.log(key + " : " + val);
+            });
+        });
+    });
+
 });
